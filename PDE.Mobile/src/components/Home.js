04fb9c2574d4726dab6fe01f386ad249a3bb1725
Juan Rene Soler <juanr.solerl_93@hotmail.es>
2022-06-05 11:02:01 -0400

    agregue las listas en las pantallas y el boton flotante de agregar

--- PDE.Mobile/src/components/Home.js
+++ PDE.Mobile/src/components/Home.js
@@ -1,5 +1,65 @@
-import { View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar } from "react-native";
+import { FAB } from 'react-native-paper';
 
-const Home = () => (<View><Text>Home</Text></View>);
+const DATA = [
+  {
+    title: "Miembros",
+    data: ["Pizza", "Burger", "Risotto", "Calzoni", "ChickenBaque", "IceCream", "CheeseCacke", "Rize", "Milk", "Beans"]
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const Home = () => (
+  <SafeAreaView style={styles.container}>
+     <SectionList
+       sections={DATA}
+       keyExtractor={(item, index) => item + index}
+       renderItem={({ item }) => <Item title={item} />}
+       renderSectionHeader={({ section: { title } }) => (
+         <Text style={styles.header}>{title}</Text>
+      )}
+    />
+    <FAB
+      style={styles.fab}
+      small
+      icon="plus"
+      onPress={() => console.log('Pressed')}
+      />
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16
+  },
+  item: {
+    backgroundColor: "#fff",
+    padding: 15,
+    marginVertical: 8
+  },
+  header: {
+    fontSize: 28,
+    color: '#fff',
+    backgroundColor: "#196F3D"
+  },
+  title: {
+    fontSize: 24
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default Home;
+
--- PDE.Mobile/src/components/Miembros.js
+++ PDE.Mobile/src/components/Miembros.js
@@ -1,5 +1,56 @@
-import { View, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar } from "react-native";
+import { FAB } from 'react-native-paper';
 
-const Miembros = () => <View><Text>prueba</Text></View>
+const DATA = [
+  {
+    data: ["Pizza", "Burger", "Risotto", "Calzoni", "ChickenBaque", "IceCream", "CheeseCacke", "Rize", "Milk", "Beans"]
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const Miembros = () => (
+  <SafeAreaView style={styles.container}>
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item title={item} />}
+    />
+    <FAB
+      style={styles.fab}
+      small
+      icon="plus"
+      onPress={() => console.log('Pressed')}
+      />
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16
+  },
+  item: {
+    backgroundColor: "#fff",
+    padding: 15,
+    marginVertical: 8
+  },
+  title: {
+    fontSize: 24
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+});
 
 export default Miembros;
+
